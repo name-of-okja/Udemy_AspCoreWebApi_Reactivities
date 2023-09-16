975ded0647b4e47d1e319a14d7e25f7a8375b31d
OkJa <gwon_hee@naver.com>
2023-09-16 14:51:36 +0900

    End of Section 12

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API;
+
+public class RegisterDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).(4,8)$", ErrorMessage = "Password must be compleate")]
+    public string Password { get; set; }
+    [Required]
+    public string DisplayName { get; set; }
+    [Required]
+    public string UserName { get; set; }
+}
