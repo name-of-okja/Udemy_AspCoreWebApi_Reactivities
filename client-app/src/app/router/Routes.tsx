63cf69db10fcf75a2781b2316da7bc46f64557c7
OkJa <gwon_hee@naver.com>
2023-10-03 14:58:48 +0900

    Client Auth

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -8,19 +8,25 @@ import TestErrors from '../../features/errors/TestError';
 import NotFound from '../../features/errors/NotFound';
 import ServerError from '../../features/errors/ServerError';
 import ProfilePage from '../../features/profiles/ProfilePage';
+import RequireAuth from './RequireAuth';
 
 export const routes: RouteObject[] = [
   {
     path: '/',
     element: <App />,
     children: [
+      {
+        element: <RequireAuth />,
+        children: [
+          { path: 'activities', element: <ActivityDashboard /> },
+          { path: 'activities/:id', element: <ActivityDetails /> },
+          { path: 'createActivity', element: <ActivityForm key='create' /> },
+          { path: 'manage/:id', element: <ActivityForm key='manage' /> },
+          { path: 'profiles/:username', element: <ProfilePage /> },
+          { path: 'errors', element: <TestErrors /> },
+        ],
+      },
       { path: '', element: <HomePage /> },
-      { path: 'activities', element: <ActivityDashboard /> },
-      { path: 'activities/:id', element: <ActivityDetails /> },
-      { path: 'createActivity', element: <ActivityForm key='create' /> },
-      { path: 'manage/:id', element: <ActivityForm key='manage' /> },
-      { path: 'profiles/:username', element: <ProfilePage /> },
-      { path: 'errors', element: <TestErrors /> },
       { path: 'not-found', element: <NotFound /> },
       { path: 'server-error', element: <ServerError /> },
       { path: '*', element: <Navigate replace to='/not-found' /> },
